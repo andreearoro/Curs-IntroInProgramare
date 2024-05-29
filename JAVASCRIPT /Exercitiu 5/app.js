9948fed447043b9227ef81b4e7d6573b99479544
andreearoro <andreearoman26@yahoo.com>
2024-05-29 18:19:48 +0000

    Actualizare

---
+++ JAVASCRIPT /Exercitiu 5/app.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    var sum = a +b 
+    console.log('Suma celor doua numere:', sum);
+}
+
+function multiply(a, b) {
+    var product = a * b;
+    console.log('Produsul celor doua numere:', product)
+}
+
+add(2,8);
+multiply(2,8);
